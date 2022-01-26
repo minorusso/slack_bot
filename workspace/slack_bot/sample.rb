f747bfd32ccb9664b94534717d3b229e64318a46
minorusso <gumbomb1@gmail.com>
2022-01-26 21:37:41 +0900

    init

---
+++ workspace/slack_bot/sample.rb
@@ -0,0 +1,12 @@
+require "slack-notify"
+require 'clockwork'
+require 'active_support/time'
+require 'dotenv'
+Dotenv.load
+module Clockwork
+    every(1.minutes, 'test') do
+        sample = SlackNotify::Client.new(webhook_url: ENV["URL"])
+        sample.notify("送れたら成功")
+        sample.notify("Another message", "#general")
+    end
+end
